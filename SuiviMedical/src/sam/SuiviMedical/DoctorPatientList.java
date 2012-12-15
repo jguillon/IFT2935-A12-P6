410b94b2d970bd93ba73f8c9aa123c877ee76f5a
samhercule <samhercule@hotmail.com>
2012-12-15 00:33:03 -0500

    TestGit

---
+++ SuiviMedical/src/sam/SuiviMedical/DoctorPatientList.java
@@ -0,0 +1,17 @@
+package sam.SuiviMedical;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DoctorPatientList extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doctormainview);
+        
+    }
+}
---
+++ SuiviMedical/src/sam/SuiviMedical/SuiviMedicalActivity.java
@@ -0,0 +1,25 @@
+package sam.SuiviMedical;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SuiviMedicalActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Button connect = (Button) findViewById(R.id.connect);
+        
+        connect.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				Intent doc = new Intent(view.getContext(), DoctorPatientList.class);
+				startActivity(doc);
+			}
+        });
+    }
+}
