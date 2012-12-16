96c56686c0e81fb6b8018d70d09132e6b2836bc6
jguillon <jeremy.damien.guillon@gmail.com>
2012-12-15 21:14:57 -0500

    Affichage de la liste des patients du docteur sur sa vue principale.

--- SuiviMedical/src/sam/SuiviMedical/DoctorPatientList.java
+++ SuiviMedical/src/sam/SuiviMedical/DoctorPatientList.java
@@ -1,17 +1,28 @@
 package sam.SuiviMedical;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DoctorPatientList extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+	private String[] patientList;
+	private ListView patientListView;
+	private ArrayAdapter<String> adapter;
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.doctormainview);
+        	
+        patientListView = (ListView) findViewById(R.id.listPatient);
+        patientList = new String[] {"Robert Duclou", "Alicia Partu", "Henri Dufour"};
+        		//  = db.query( <Nom et prénom de tous les patients> );
         
-    }
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, patientList);
+        patientListView.setAdapter(adapter);
+	}
 }
--- SuiviMedical/src/sam/SuiviMedical/SuiviMedicalActivity.java
+++ SuiviMedical/src/sam/SuiviMedical/SuiviMedicalActivity.java
@@ -7,13 +7,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class SuiviMedicalActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override //Test 1, 2
-    public void onCreate(Bundle savedInstanceState) {
+	
+	Button connect;
+
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        Button connect = (Button) findViewById(R.id.connect);
+        connect = (Button) findViewById(R.id.connect);
         
         connect.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
