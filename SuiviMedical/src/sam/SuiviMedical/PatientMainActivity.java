522d866d1129464fe1da10045a29ce869775abf3
Fred <Fred@Fred-THINK>
2012-12-15 20:04:24 -0500

    PatientMain

---
+++ SuiviMedical/src/sam/SuiviMedical/PatientMainActivity.java
@@ -0,0 +1,15 @@
+package sam.SuiviMedical;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class PatientMainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.patientmain);
+    }
+
+}
