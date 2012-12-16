8df20cd28b4d0252e062964773615551ee04ef49
jguillon <jeremy.damien.guillon@gmail.com>
2012-12-15 23:49:43 -0500

    Ajout de la classe DoctorPatientEventList.java

---
+++ SuiviMedical/src/sam/SuiviMedical/DoctorPatientEventList.java
@@ -0,0 +1,11 @@
+package sam.SuiviMedical;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DoctorPatientEventList extends Activity {
+	 public void onCreate(Bundle savedInstanceState) {
+		 super.onCreate(savedInstanceState);
+		 setContentView(R.layout.doctoreventlist);
+	 }
+}
--- SuiviMedical/src/sam/SuiviMedical/DoctorPatientList.java
+++ SuiviMedical/src/sam/SuiviMedical/DoctorPatientList.java
@@ -1,9 +1,7 @@
 package sam.SuiviMedical;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -19,18 +17,17 @@ public class DoctorPatientList extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.doctormainview);
-        	
+        //Initialisation des variables
         patientListView = (ListView) findViewById(R.id.listPatient);
         patientList = new String[] {"Robert Duclou", "Alicia Partu", "Henri Dufour"};
         		//  = db.query( <Nom et prénom de tous les patients> );
-        
-        adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, patientList);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, patientList);
+        //Paramétrisation
         patientListView.setAdapter(adapter);
         patientListView.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				
+			public void onItemClick(AdapterView<?> arg0, View view, int arg2, long arg3) {
+				Intent doc = new Intent(view.getContext(), DoctorPatientEventList.class);
+				startActivity(doc);
 			}        	
         });
 	}
