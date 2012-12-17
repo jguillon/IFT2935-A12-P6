84e6df9e026cf8bd8ef1b535e6ae1f29416abc91
sandra <sandra>
2012-12-17 10:20:29 -0500

    update de R.java

--- SuiviMedical/src/sam/SuiviMedical/DoctorPatientList.java
+++ SuiviMedical/src/sam/SuiviMedical/DoctorPatientList.java
@@ -3,7 +3,7 @@ package sam.SuiviMedical;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.View; 
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
--- SuiviMedical/src/sam/SuiviMedical/SuiviMedicalActivity.java
+++ SuiviMedical/src/sam/SuiviMedical/SuiviMedicalActivity.java
@@ -1,5 +1,6 @@
 package sam.SuiviMedical;
 
+import sam.SuiviMedical.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
