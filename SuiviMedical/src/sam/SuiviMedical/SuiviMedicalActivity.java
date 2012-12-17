bde4b337ea91a475d057ced0817c33987a4fd70e
Fred <Fred@Fred-THINK>
2012-12-17 07:41:39 -0500

    jkh

--- SuiviMedical/src/sam/SuiviMedical/SuiviMedicalActivity.java
+++ SuiviMedical/src/sam/SuiviMedical/SuiviMedicalActivity.java
@@ -1,6 +1,9 @@
 package sam.SuiviMedical;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -16,6 +19,23 @@ public class SuiviMedicalActivity extends Activity {
         super.onCreate(savedInstanceState);
     	//EditText loginEdit = (EditText) findViewById(R.id.userName);
         setContentView(R.layout.main);
+        AlertDialog.Builder simple = new AlertDialog.Builder(this);
+        simple.setMessage("C'est aussi simple que ca ?");
+        simple.setCancelable(false);
+        simple.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+        	 public void onClick(DialogInterface dialog, int id) {
+        	        finish();
+        	 }
+        	 });
+        simple.setNegativeButton("No", new DialogInterface.OnClickListener() {
+        	        public void onClick(DialogInterface dialog, int id) {
+        	        //  Action for 'NO' Button
+        	        dialog.cancel();
+        	    }
+       });
+        simple.create();
+        simple.show();
+        	  
         
         Button connect = (Button) findViewById(R.id.connect);
         final EditText loginEdit = (EditText) findViewById(R.id.userName);
