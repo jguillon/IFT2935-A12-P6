a60d3f6beec6cdad3d07b782e7eeef24c54f4335
jguillon <jeremy.damien.guillon@gmail.com>
2012-12-18 14:55:22 -0500

    Première utilisation de la DB.

--- SuiviMedical/src/sam/SuiviMedical/SuiviMedicalActivity.java
+++ SuiviMedical/src/sam/SuiviMedical/SuiviMedicalActivity.java
@@ -6,91 +6,68 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import database.DataSource;
 import doctorActivities.PatientsList;
 
-public class SuiviMedicalActivity extends Activity {
+public class SuiviMedicalActivity extends Activity implements OnClickListener {
 
 	Infos session;
 	/** Called when the activity is first created. */
-	private EditText loginEdit;
+	private EditText loginET, passwordET;
+	private Button connectB;
 	private DataSource ds;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// EditText loginEdit = (EditText) findViewById(R.id.userName);
 		setContentView(R.layout.main);
 
-		ds = new DataSource(this);
-		Button connect = (Button) findViewById(R.id.connect);
-		final EditText loginEdit = (EditText) findViewById(R.id.userName);
+		connectB 	= (Button) 	 findViewById(R.id.connect);
+		loginET 	= (EditText) findViewById(R.id.userName);
+		passwordET	= (EditText) findViewById(R.id.editPassword);
 
-		connect.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				String login = Login();
-				Log.w("login ",login);
-				ds.open();
-				Cursor roleC = ds.selectWhere(DataSource.TBL_ROLE, "Role", "NoAss = "+login);
-				roleC.moveToFirst();
-				String role = roleC.getString(roleC.getColumnIndex("Role"));
-				ds.close();
-				if (roleC != null && role.equals("Patient")) {
-					Intent doc = new Intent(view.getContext(),
-							PatientMainActivity.class);
-					doc.putExtra("session", session);
-					startActivity(doc);
-				} else if (roleC != null && role.equals("Doctor")) {
-					Intent doc = new Intent(view.getContext(),
-							PatientsList.class);
-					doc.putExtra("session", session);
-					startActivity(doc);
-				} else {
-					// EditText loginEdit = (EditText)
-					// findViewById(R.id.userName);
-					EditText pswEdit = (EditText) findViewById(R.id.editPassword);
-					loginEdit.setTextColor(Color.RED);
-					pswEdit.setText("");
-				}
-			}
-		});
-		loginEdit.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				loginEdit.setTextColor(Color.BLACK);
-			}
-		});
+		ds = new DataSource(this);
+
+		connectB.setOnClickListener(this);
 	}
 
 	@Override
 	public void onResume() {
 		super.onResume();
-		if (session != null && loginEdit != null) {
+		if (session != null && loginET != null) {
 			session.clearSession();
-			loginEdit.setText("");
+			loginET.setText("");
 		}
 	}
 
-	public String Login() {
-		EditText loginEdit = (EditText) findViewById(R.id.userName);
-		String login = loginEdit.getText().toString();
-		// Cursor c =
-		// db.rawQuery("select Role.PersonRole from Role where Role.NoAss = ?",
-		// login);
-		// c.moveToFirst();
-		// String role = c.getString(1);
-		if (login.equals("Patient")) {
-			session = new Infos("PATT09809830", "Patient");
-			return /* role */"Patient";
-		} else if (login.equals("Doctor")) {
-			session = new Infos("DOCT09809830", "Doctor");
-			return "Doctor";
-		} else
-			return "";
+	@Override
+	public void onClick(View v) {
+		ds.open();
+		
+		String login = loginET.getText().toString();
+		String pswd = passwordET.getText().toString();
+		Cursor roleC = ds.selectWhere(DataSource.TBL_ROLE, "PersonRole", "NoAss = \""+login+"\"");
+		
+		if(roleC != null && roleC.getCount() != 0 && pswd == login) {
+			roleC.moveToFirst();
+			String role = roleC.getString( roleC.getColumnIndex("PersonRole") );
+			ds.close();
+			session = new Infos(login, role);
+			if (role.equals("Patient")) {
+				Intent i = new Intent(v.getContext(), PatientMainActivity.class);
+				i.putExtra("session", session);
+				startActivity(i);
+			} else if (role.equals("Doctor")) {
+				Intent i = new Intent(v.getContext(), PatientsList.class);
+				i.putExtra("session", session);
+				startActivity(i);
+			}
+		}
+		loginET.setTextColor(Color.RED);
+		passwordET.setText("");
 	}
 }
